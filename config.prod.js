f4112e1efe321206491bd75ccfddda6f2d238a74
135-pepsi <only135cc@gmail.com>
2026-02-28 21:20:01 +0800

    Add files via upload

--- config.prod.js
+++ config.prod.js
@@ -1,6 +1,6 @@
 window.MES_CONFIG = {
-  SUPABASE_URL: "https://fjzduvdkqfqbwaqxegli.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_iYFIDPVc-_R3Z-smBpseNw_C3zW3SHD",
+  SUPABASE_URL: "https://hhvvlyhvcttzubxiczpl.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_kFsXHtTUreZ6OqQ5qlWI1A_VheVcz71",
   AUTO_REFRESH_SECONDS: 5,
   SUPABASE_STORAGE_BUCKET: "material-screenshots",
   SUPABASE_STORAGE_SIGNED_EXPIRES: 3600,
